925abb13e50f38f88274c3e2f4c1aec80b52b414
curtis <cyber44@kali-vg.attlocal.net>
2026-04-01 16:24:50 -0400

    feat: add stryker sandbox and test isolation setup

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,17 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+  resetModules: true,
+  forceExit: true,
+  testTimeout: 30000,
   collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.{ts,js}',
     '!src/**/*.d.ts',
   ],
   coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
 };
