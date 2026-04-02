2e3ac41d10886d6757b2cb84add7319296e19583
curtis <cyber44@kali-vg.attlocal.net>
2026-04-01 20:28:31 -0400

    test: add rapid mutation hunt and jest compatibility mock

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
+  roots: ['<rootDir>/test', '<rootDir>/tests'],
   clearMocks: true,
   resetMocks: true,
   restoreMocks: true,
   resetModules: true,
   forceExit: true,
   testTimeout: 30000,
+  moduleNameMapper: {
+    '^node-fetch$': '<rootDir>/test/__mocks__/node-fetch.js',
+  },
   collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.{ts,js}',
